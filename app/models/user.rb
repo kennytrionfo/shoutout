98a5691126010f3a57ede4833c86b2e0a8d4efc9
kennytrionfo <kentrionfo@gmail.com>
2014-06-25 16:07:43 -0600

    next

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :mood, presence: true
 
+  def by_mood
 
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,23 @@ RSpec.describe User, :type => :model do
   describe 'validations' do
       it { should validate_presence_of :first_name }
       it { should validate_presence_of :last_name }
+      it { should validate_presence_of :mood }
   end
 
   describe 'Find user by mood' do
+    describe '.by_mood' do
+      before do
+        create(:user, first_name: 'Barbie', last_name: 'Doll', mood: 'Super Happy')
+        create(:user, first_name: 'Ken', last_name: 'Dude', mood: 'Really Happy')
+        create(:user, first_name: 'Buzz', last_name: 'Lighyear', mood: 'Hyper')
+        end
     it 'Should find user by mood' do
-      expect(User.find)
+      expect(User.by_mood('happy'))
+    end
+
+        end
+      end
+
+
 
 end
