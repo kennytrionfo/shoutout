d1683a9aa9b0a6233e802ee5876aadd6b7ea50fa
kennytrionfo <kentrionfo@gmail.com>
2014-06-26 21:56:45 -0600

    edit and destroy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe UsersController, :type => :controller do
     before do
       @user = create(:user, first_name: "Buzz")
     end
-
     it 'shows one user & render the show template' do
       get :show, id: @user.id
       expect(response).to be_success
@@ -26,28 +25,60 @@ RSpec.describe UsersController, :type => :controller do
     end
   end
 
-  describe '#new' , :focus do
+  describe '#new'  do
     it 'sets up a new user instance' do
-      
+      get :new
+      expect(response).to be_success
+      expect(assigns(:user)).to be_new_record
+      expect(response).to render_template('new')
     end
   end
 
-  describe '#create' do
-
+  describe '#create'  do
+    context 'when saving a valid user' do
+      it 'creates a new user and saves it to the db' do
+        expect{
+          post :create, user: {first_name: 'Dizzy', last_name: 'Wizzy', mood: 'crazy'}
+        }.to change(User, :count).by(1)
+      end
+    end
+    context 'when the user fails to save' do
+      it 'fails to save to db' do
+        post :create, user: {string: nil}
+        expect(response).to render_template('new')
+        expect(User.count).to eq 0
+      end
+    end
   end
 
   describe '#edit' do
-
+    before do
+      @user = create(:user)
+    end
+    it 'displays the note that I want to edit' do
+      get :edit, id: @user.id
+      expect(response).to be_success
+      expect(assigns(:user).id).to eq @user.id
+      expect(response).to render_template('edit')
+    end
   end
 
   describe '#udpate' do
 
   end
 
-  describe '#destroy' do
+  describe '#destroy' , :focus do
+    before do
+      @user = create(:user)
+    end
+    it 'removes the note from the database' do
+       expect {
+         delete :destroy, id: @user.id
+       }.to change(User, :count).from(1).to(0)
+       expect(response).to be_redirect
+       expect(response).to redirect_to user_path
+    end
 
   end
-
-
 
 end
